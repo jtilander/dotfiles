cfa0fe4d36942c181791d0491b71e20c793e1449
Jim Tilander <jim.tilander@activision.com>
2017-12-19 09:28:03 -0800

    Guard linux stuff on Darwin

--- sublime/install.sh
+++ sublime/install.sh
@@ -30,15 +30,17 @@ if [ ! -d "$prefix" ]; then
 fi
 
 
-if [ ! -f "$prefix/.package_installed" ]; then
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get -y install wget apt-transport-https
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo apt-get -y update
-    sudo apt-get -y install sublime-text
+if [ "$(uname -s)" != "Darwin" ]; then
+    if [ ! -f "$prefix/.package_installed" ]; then
+        export DEBIAN_FRONTEND=noninteractive
+        sudo apt-get -y install wget apt-transport-https
+        wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+        echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+        sudo apt-get -y update
+        sudo apt-get -y install sublime-text
 
-    touch "$prefix/.package_installed"
+        touch "$prefix/.package_installed"
+    fi
 fi
 
 
