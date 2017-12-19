d9ac2dcf19aa795ab7fced6cd7ecc9b6166571bf
Jim Tilander <jim.tilander@activision.com>
2017-12-19 10:28:49 -0800

    Fixed sublime package control install.

--- sublime/install.sh
+++ sublime/install.sh
@@ -11,14 +11,15 @@ fi
 
 
 function symlink_preference() {
-	name=$1
-	original="$prefix/Packages/User/$name"
+	component=$1
+	name=$2
+	original="$prefix/Packages/$component/$name"
 	new="$HOME/.dotfiles/sublime/$name"
-    if [ ! -d "$prefix/Packages/User" ]; then
-        mkdir -p "$prefix/Packages/User"
-    fi
+	if [ ! -d "$prefix/Packages/$component" ]; then
+		mkdir -p "$prefix/Packages/$component"
+	fi
 	if [ -f "$original" ]; then
-     	rm "$original"
+		rm "$original"
 	fi
 	if [ ! -h "$original" ]; then
 		ln -s "$new" "$original"
@@ -26,34 +27,34 @@ function symlink_preference() {
 }
 
 if [ ! -d "$prefix" ]; then
-    mkdir -p "$prefix"
+	mkdir -p "$prefix"
 fi
-
 
 if [ "$(uname -s)" != "Darwin" ]; then
-    if [ ! -f "$prefix/.package_installed" ]; then
-        export DEBIAN_FRONTEND=noninteractive
-        sudo apt-get -y install wget apt-transport-https
-        wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-        echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-        sudo apt-get -y update
-        sudo apt-get -y install sublime-text
+	if [ ! -f "$prefix/.package_installed" ]; then
+		export DEBIAN_FRONTEND=noninteractive
+		sudo apt-get -y install wget apt-transport-https
+		wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+		echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+		sudo apt-get -y update
+		sudo apt-get -y install sublime-text
 
-        touch "$prefix/.package_installed"
-    fi
+		touch "$prefix/.package_installed"
+	fi
 fi
 
 
-echo 'Linking SublimeText preferences'
 if [ ! -f "$prefix/Installed Packages/Package Control.sublime-package" ]; then
 	if [ ! -d "$prefix/Installed Packages" ]; then
-        mkdir -p "$prefix/Installed Packages"
-    fi
-    curl -sSL -f "https://packagecontrol.io/Package%20Control.sublime-package" > "$prefix/Installed Packages/Package Control.sublime-package"
+		mkdir -p "$prefix/Installed Packages"
+	fi
+    echo 'Downloading package control seed'
+	curl -sSL -f "https://packagecontrol.io/Package%20Control.sublime-package" > "$prefix/Installed Packages/Package Control.sublime-package"
 fi
-symlink_preference 'Preferences.sublime-settings'
-symlink_preference 'Anaconda.sublime-settings'
-symlink_preference 'Package Control.sublime-settings'
-symlink_preference "$keymap"
+echo 'Linking SublimeText preferences'
+symlink_preference 'User' 'Preferences.sublime-settings'
+symlink_preference 'User' 'Anaconda.sublime-settings'
+symlink_preference 'User' 'Package Control.sublime-settings'
+symlink_preference 'User' "$keymap"
 
 
