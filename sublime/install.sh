dc03ef6083917e378fdf70ea024960af7d3ddda3
Jim Tilander <jim.tilander@activision.com>
2017-12-18 21:36:07 -0800

    Fixed some linux installation

--- sublime/install.sh
+++ sublime/install.sh
@@ -14,6 +14,9 @@ function symlink_preference() {
 	name=$1
 	original="$prefix/Packages/User/$name"
 	new="$HOME/.dotfiles/sublime/$name"
+    if [ ! -d "$prefix/Packages/User" ]; then
+        mkdir -p "$prefix/Packages/User"
+    fi
 	if [ -f "$original" ]; then
      	rm "$original"
 	fi
@@ -22,9 +25,29 @@ function symlink_preference() {
 	fi
 }
 
+if [ ! -d "$prefix" ]; then
+    mkdir -p "$prefix"
+fi
+
+
+if [ ! -f "$prefix/.package_installed" ]; then
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get -y install wget apt-transport-https
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt-get -y update
+    sudo apt-get -y install sublime-text
+
+    touch "$prefix/.package_installed"
+fi
+
+
 echo 'Linking SublimeText preferences'
 if [ ! -f "$prefix/Installed Packages/Package Control.sublime-package" ]; then
-	curl -s -f "https://packagecontrol.io/Package%20Control.sublime-package" > "$prefix/Installed Packages/Package Control.sublime-package"
+	if [ ! -d "$prefix/Installed Packages" ]; then
+        mkdir -p "$prefix/Installed Packages"
+    fi
+    curl -sSL -f "https://packagecontrol.io/Package%20Control.sublime-package" > "$prefix/Installed Packages/Package Control.sublime-package"
 fi
 symlink_preference 'Preferences.sublime-settings'
 symlink_preference 'Anaconda.sublime-settings'
