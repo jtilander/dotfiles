86eed1a439a6e1a5eb040ade3c9ad783c02d4655
Jim Tilander <jim@tilander.org>
2018-02-10 21:49:31 -0800

    Added alias for dir

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,10 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 alias cd..='cd ..'
+alias dir='ls -l'
 
 alias colourify='grc /usr/local/bin/grc -es --colour=auto' # fix broken alias
 
 alias kc=kubectl
+
+
