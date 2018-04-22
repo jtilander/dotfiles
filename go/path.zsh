22cb3b3c06c7e360a288f431dedb36a7c2aa24fc
Jim Tilander <jim@tilander.org>
2018-04-22 10:14:26 -0700

    Conditionally check the gopath

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,10 @@
-export GOPATH=$PROJECTS/go
+
+if [ -d $HOME/go ]; then
+	export GOPATH=$HOME/go	
+fi
+
+if [ -d $PROJECTS/go ]; then
+	export GOPATH=$PROJECTS/go
+fi
+
 export PATH="$GOPATH/bin:$PATH"
