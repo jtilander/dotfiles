33088ea07c2d7d95742517e3dfe84ba1e7107034
Jim Tilander <jim@tilander.org>
2018-07-16 12:49:56 -0700

    Updated gopath

--- go/path.zsh
+++ go/path.zsh
@@ -1,10 +1,14 @@
 
 if [ -d $HOME/go ]; then
 	export GOPATH=$HOME/go	
+	export GOBIN=$GOPATH/bin
+	mkdir -p $GOBIN
 fi
 
 if [ -d $PROJECTS/go ]; then
 	export GOPATH=$PROJECTS/go
+	export GOBIN=$GOPATH/bin
+	mkdir -p $GOBIN
 fi
 
 export PATH="$GOPATH/bin:$PATH"
