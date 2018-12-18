ba42ff869372996c05b661120cbc51c9ba90145e
Jim Tilander <jim@tilander.org>
2018-12-17 22:49:56 -0800

    Replaced ll with exa

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,8 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 alias cd..='cd ..'
-alias dir='ls -l'
-
+alias ll='exa --long --header --extended --git'
+alias dir='exa --long --header --extended --git'
 alias colourify='grc /usr/local/bin/grc -es --colour=auto' # fix broken alias
 
 alias kc=kubectl
