37bf46602e1df6006070fd9204db93b3fec7eb7b
Jim Tilander <jim@tilander.org>
2020-06-15 08:03:43 -0700

    Fixed aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,8 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 alias cd..='cd ..'
-alias ll='exa --long --header --git --color=never'
-alias ls='exa --git --color=never'
-alias dir='exa --long --header --extended --git'
+alias ll='ls -l'
+alias dir='ls -l'
 alias colourify='grc /usr/local/bin/grc -es --colour=auto' # fix broken alias
 
 alias kc=kubectl
