7cc8de23ba20491e1c7f2419ae6d6df849797bb3
Jim Tilander <jim@tilander.org>
2024-05-27 14:37:26 -0700

    Updated setting

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,8 +1,8 @@
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
-  source `brew --prefix`/etc/grc.zsh
-fi
+#if (( $+commands[grc] )) && (( $+commands[brew] ))
+#then
+#  source `brew --prefix`/etc/grc.zsh
+#fi
 
 # We don't want to do this for `make`, to disable the buffering.
-unset -f make
+# unset -f make
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias cd..='cd ..'
 alias ll='ls -l'
 alias dir='ls -l'
-alias colourify='grc /usr/local/bin/grc -es --colour=auto' # fix broken alias
+#alias colourify='grc /usr/local/bin/grc -es --colour=auto' # fix broken alias
 
 alias kc=kubectl
 
